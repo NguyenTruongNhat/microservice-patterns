fa595fb00e01712a88a95b64c84bec1a45fff4d8
NguyenTruongNhat <nguyentruongnhat9599@gmail.com>
2025-10-28 22:30:36 +0700

    #saga-or: add ticket service

---
+++ patterns/Saga/Orchestrtion/Saga.TripPlanner.TicketService/Bootstraping/ApplicationServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saga.TripPlanner.TicketService.Infrastructure.Data;
+
+namespace Saga.TripPlanner.TicketService.Bootstraping
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IHostApplicationBuilder AddApplicationServices(this IHostApplicationBuilder builder)
+        {
+            builder.Services.AddOpenApi();
+
+            // Add EF Core
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<TicketDbContext>(options => options.UseNpgsql(connectionString));
+
+            return builder;
+        }
+
+    }
+}
---
+++ patterns/Saga/Orchestrtion/Saga.TripPlanner.TicketService/Infrastructure/Data/TicketDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Saga.TripPlanner.TicketService.Infrastructure.Entity;
+
+namespace Saga.TripPlanner.TicketService.Infrastructure.Data;
+public class TicketDbContext(DbContextOptions<TicketDbContext> options) : DbContext(options)
+{
+    public DbSet<Ticket> Tickets { get; set; } = default!;
+    public DbSet<TicketType>  TicketTypes { get; set; } = default!;
+}
